d61c4e5e7a23e692f2da8e630f008a633bc5c4db
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-07 20:35:16 +0100

    added font

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-$(function() {
-  $("button").on({
-    mouseover: function() {
-      $(this).css({
-        left: (Math.random() * 200) + "px",
-        top: (Math.random() * 200) + "px",
-      });
-    }
-  });
+const button = document.querySelector(".celebration-button");
+const counter = document.querySelector("body");
+let count = 0;
+
+button.addEventListener("click", () => {
+  count++;
+  counter.textContent = count;
+});
+
+const message = document.getElementById("message");
+const bodyArea = document.getElementById("body");
+
+const messages = [
+  "Oh. You thought you had it, didn't you?",
+  "You're supposed to click the button, not me!",
+  "Try again!",
+];
+
+bodyArea.addEventListener("click", () => {
+  const randomElement = messages[Math.floor(Math.random() * messages.length)];
+  message.innerHTML = randomElement;
+});
+
+const themeSwitch = document.querySelector("input");
+
+themeSwitch.addEventListener("change", () => {
+  document.body.classList.toggle("dark-theme");
 });
