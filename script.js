8737dd7b775158da00622756ea55e35574d3cab3
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-08 09:45:53 +0100

    modified .prettierrc

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const button = document.querySelector(".celebration-button");
-const counter = document.querySelector("body");
+const button = document.querySelector('.celebration-button');
+const counter = document.querySelector('body');
 let count = 0;
 
-button.addEventListener("click", () => {
+button.addEventListener('click', () => {
   count++;
   counter.textContent = count;
 });
 
-const message = document.getElementById("message");
-const bodyArea = document.getElementById("body");
+const message = document.getElementById('message');
+const bodyArea = document.getElementById('body');
 
 const messages = [
   "Oh. You thought you had it, didn't you?",
   "You're supposed to click the button, not me!",
-  "Try again!",
+  'Try again!',
 ];
 
-bodyArea.addEventListener("click", () => {
+bodyArea.addEventListener('click', () => {
   const randomElement = messages[Math.floor(Math.random() * messages.length)];
   message.innerHTML = randomElement;
 });
 
-const themeSwitch = document.querySelector("input");
+const themeSwitch = document.querySelector('input');
 
-themeSwitch.addEventListener("change", () => {
-  document.body.classList.toggle("dark-theme");
+themeSwitch.addEventListener('change', () => {
+  document.body.classList.toggle('dark-theme');
 });
